7cac44bbe463c706b9b5119c1a1b7d40ed61a23d
Vi Anh Tuan <vituan121002@gmail.com>
2024-04-07 22:57:45 +0700

    add order model

--- migration/migration.js
+++ migration/migration.js
@@ -51,6 +51,8 @@ function createTable_Seller(opt){
 		setTimeout(() => createdTable_Category(opt), 2000);
 	}
 }
+
+
 function createdTable_Category(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Category (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -83,6 +85,21 @@ function createdTable_Product(opt) {
 		setTimeout(() => createdTable_Order(opt), 2000);
 	}
 }
+
+// function createdTable_ShippingInfo(opt){
+//     const sql = `CREATE TABLE shipping_info (
+//         id INT(11) NOT NULL AUTO_INCREMENT,
+//         address VARCHAR(255),
+//         phoneNo VARCHAR(20) ,
+//         city VARCHAR(100) ,
+//         PRIMARY KEY (id)
+//       );
+//       `;
+//       if (opt === true){
+//         query(sql);
+//         setTimeout(() => createdTable_Order, 2000);
+//       }
+// }
 
 function createdTable_Order(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Orders (
