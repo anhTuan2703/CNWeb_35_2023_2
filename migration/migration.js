2efcee02a919e500bf10d99362876d2731b92183
iamTuans <tuan200345@gmail.com>
2024-04-21 18:04:25 +0700

    new

--- migration/migration.js
+++ migration/migration.js
@@ -13,29 +13,36 @@ function createTable_Profile(opt) {
       `;
 	if (opt === true) {
 		query(sql);
+        setTimeout(() => console.log("create table profile"), 500);
 		setTimeout(() => createdTable_Account(opt), 2000);
 	}
 }
 function createdTable_Account(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Account (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        profile_id INT(20) NOT NULL,
         account_name VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
-        created_at TIMESTAMP NOT NULL
+        created_at TIMESTAMP NOT NULL,
+        FOREIGN KEY (profile_id) REFERENCES Profile(id) ON DELETE CASCADE
     );    
     `;
 	if (opt === true) {
 		query(sql);
+        setTimeout(() => console.log("create table account"), 500);
 		setTimeout(() => createTable_Customer(opt), 2000);
 	}
 }
 function createTable_Customer(opt){
     const sql = `CREATE TABLE IF NOT EXISTS Customer (
-        id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT
+        id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        account_id INT(20) NOT NULL,
+        FOREIGN KEY (account_id) REFERENCES Account(id) ON DELETE CASCADE
     );
     `;
     if (opt === true) {
 		query(sql);
+        setTimeout(() => console.log("create table customer"), 500);
 		setTimeout(() => createTable_Seller(opt), 2000);
 	}
 }
@@ -43,16 +50,17 @@ function createTable_Seller(opt){
     const sql = `CREATE TABLE IF NOT EXISTS Seller (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
         shop_name TEXT,
-        description TEXT
+        description TEXT,
+        account_id INT(20) NOT NULL,
+        FOREIGN KEY (account_id) REFERENCES Account(id) ON DELETE CASCADE
     );    
     `;
     if (opt === true) {
 		query(sql);
+        setTimeout(() => console.log("create table seller"), 500);
 		setTimeout(() => createdTable_Category(opt), 2000);
 	}
 }
-
-
 function createdTable_Category(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Category (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -61,10 +69,10 @@ function createdTable_Category(opt) {
     `;
 	if (opt === true) {
 		query(sql);
+        setTimeout(() => console.log("create table category"), 500);
 		setTimeout(() => createdTable_Product(opt), 2000);
 	}
 }
-
 function createdTable_Product(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Product (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -77,36 +85,60 @@ function createdTable_Product(opt) {
         description TEXT,
         number INT,
         rating FLOAT,
-        created_at TIMESTAMP NOT NULL
+        created_at TIMESTAMP NOT NULL,
+        FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE,
+        FOREIGN KEY (seller_id) REFERENCES Seller(id) ON DELETE CASCADE
     );    
     `;
 	if (opt === true) {
 		query(sql);
-		setTimeout(() => createdTable_Order(opt), 2000);
+        setTimeout(() => console.log("create table product"), 500);
+		setTimeout(() => createdTable_ShippingInfo(opt), 2000);
 	}
 }
-
-// function createdTable_ShippingInfo(opt){
-//     const sql = `CREATE TABLE shipping_info (
-//         id INT(11) NOT NULL AUTO_INCREMENT,
-//         address VARCHAR(255),
-//         phoneNo VARCHAR(20) ,
-//         city VARCHAR(100) ,
-//         PRIMARY KEY (id)
-//       );
-//       `;
-//       if (opt === true){
-//         query(sql);
-//         setTimeout(() => createdTable_Order, 2000);
-//       }
-// }
-
+function createdTable_ShippingInfo(opt){
+    const sql = `CREATE TABLE IF NOT EXISTS Shipping_info (
+        id INT(20) NOT NULL AUTO_INCREMENT,
+        address VARCHAR(255),
+        phoneNo VARCHAR(20) ,
+        city VARCHAR(100) ,
+        PRIMARY KEY (id)
+      );
+      `;
+      if (opt === true){
+        query(sql);
+        setTimeout(() => console.log("create table shipping info"), 500);
+        setTimeout(() => createdTable_Order(opt), 2000);
+      }
+}
 function createdTable_Order(opt) {
 	const sql = `CREATE TABLE IF NOT EXISTS Orders (
         id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-        product_id INT(20) NOT NULL,
         customer_id INT(20) NOT NULL,
-        created_at TIMESTAMP NOT NULL
+        ship_price DECIMAL (13, 2),
+        total_price DECIMAL (13, 2),
+        ship_id INT(20),
+        orderStatus VARCHAR(255),
+        created_at TIMESTAMP NOT NULL,
+        FOREIGN KEY (customer_id) REFERENCES Customer(id) ON DELETE CASCADE,
+        FOREIGN KEY (ship_id) REFERENCES Shipping_info(id) ON DELETE CASCADE
+    );
+    `;
+	if (opt === true) {
+		query(sql);
+        setTimeout(() => console.log("create table order"), 500);
+		setTimeout(() => createdTable_ItemOrder(opt), 2000);
+	}
+}
+function createdTable_ItemOrder(opt){
+    const sql = `CREATE TABLE IF NOT EXISTS ItemOrder (
+        id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        product_id INT(20) NOT NULL,
+        order_id INT(20) NOT NULL,
+        amount INT(10) NOT NULL,
+        created_at TIMESTAMP NOT NULL,
+        FOREIGN KEY (order_id) REFERENCES Orders(id) ON DELETE CASCADE,
+        FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE
     );
     `;
 	if (opt === true) {
