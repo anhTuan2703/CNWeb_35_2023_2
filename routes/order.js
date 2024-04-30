e981ca607a898c73595903f3293f340e42e8ad8f
Vi Anh Tuan <vituan121002@gmail.com>
2024-04-30 17:18:41 +0700

    api: add to cart, update amount, update shipping info

--- routes/order.js
+++ routes/order.js
@@ -7,7 +7,11 @@ const {
 	updateOrder,
 	deleteOrder,
 	deleteItemOrder,
+	getOrderDetail,
+	addProduct,
+	updateShippingInfo,
 	getOrders,
+	updateProductAmount,
 	// getOrderById,
 	// getUserOrders,
 } = require("../controllers/order.controller");
@@ -19,6 +23,10 @@ router.put("/order/:id", isAuthenticatedUser, updateOrder);
 // router.get("/order/:id", isAuthenticatedUser, getOrderById);
 router.delete("/order/:id", isAuthenticatedUser, deleteOrder);
 router.delete("/order/:orderId/product/:productId", isAuthenticatedUser, deleteItemOrder);
+router.get("/order/detail/:orderId", isAuthenticatedUser, getOrderDetail);
+router.post("/order/:orderId/product/:productId", isAuthenticatedUser, addProduct);
+router.put("/order/updateItem/:orderId", isAuthenticatedUser, updateProductAmount);
+router.put("/order/shippingInfo/:orderId", isAuthenticatedUser, updateShippingInfo);
 //router.get("/user/orders", isAuthenticatedUser, getUserOrders);
 
 module.exports = router;
