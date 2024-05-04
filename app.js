72d196dd115888936ff37a99080d5641f6ed0471
iamTuans <tuan200345@gmail.com>
2024-05-04 12:17:43 +0700

    Fix app.js

--- app.js
+++ app.js
@@ -1,20 +1,6 @@
 //require('./migration/migration.js')
 
 const express = require('express');
-<<<<<<< HEAD
-const { query } = require("./database/database.js")
-const app = express();
-const app_config = require('./configs/app.config');
-const router = require('./routes/router');
-const port = app_config.port || 3000;
-
-app.use(express.json({
-  limit: '2mb'
-}));
-app.use(express.urlencoded({
-  extended: true
-}));
-=======
 const middleError = require("./middlewares/errors");
 const app = express();
 const port = 3000;
@@ -23,8 +9,9 @@ app.use(
 		extended: true,
 	}),
 );
-app.use(express.json());
->>>>>>> 6d1d76d89615e9581876f958704e5453ccb7ea0b
+app.use(express.json({
+  limit: '2mb'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -34,20 +21,8 @@ app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
 });
 
-<<<<<<< HEAD
-app.use('/api/v1', router);
-
-(async () => {
-  try {
-    const results = await query('SELECT * FROM Profile');
-    console.log("data length: " + results.length);
-  } catch (error) {
-    console.error('Error:', error);
-  }
-})();
-=======
 const orderRoute = require("./routes/order");
+app.use("/api/v1", require("./routes/router"))
 app.use("/api/v1", orderRoute);
 
 app.use(middleError);
->>>>>>> 6d1d76d89615e9581876f958704e5453ccb7ea0b
